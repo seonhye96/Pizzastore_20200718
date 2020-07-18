5c49e29010624c0e21959254ffc55bcb1987a499
황선혜 <ghkdtjs6451@gmail.com>
2020-07-18 14:40:46 +0900

    내 정보 프래그먼트 추가

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
@@ -1,7 +1,24 @@
 package kr.co.tjoeun.pizzastore_20200718.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.pizzastore_20200718.R
 
 class MyProfileFragment:Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
 }
