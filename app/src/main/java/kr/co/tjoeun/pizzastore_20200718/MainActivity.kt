c441ff8910ba7a661f8508722a83adb5fae62b03
황선혜 <ghkdtjs6451@gmail.com>
2020-07-18 14:33:22 +0900

    뷰페이저 XML 배치

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/MainActivity.kt
@@ -3,9 +3,20 @@ package kr.co.tjoeun.pizzastore_20200718
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setupValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setupValues() {
+
     }
 }
---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,7 @@
+package kr.co.tjoeun.pizzastore_20200718.fragments
+
+import androidx.fragment.app.Fragment
+
+class PizzaStoreListFragment:Fragment() {
+
+}
