4f715907c732cd595b591dd2810f2fd4d072ea5e
황선혜 <ghkdtjs6451@gmail.com>
2020-07-18 15:17:00 +0900

    피자가게 어댑터 뼈대 작업 진행

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/adapters/PizzaStoreAdapter.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.co.tjoeun.pizzastore_20200718.R
 import kr.co.tjoeun.pizzastore_20200718.datas.PizzaStore
 
-class PizzaViewPagerAdapter(val mContext:Context, val resId:Int, val mList : List<PizzaStore>) : ArrayAdapter<PizzaStore>(mContext, resId, mList) {
+class PizzaStoreAdapter(val mContext:Context, val resId:Int, val mList : List<PizzaStore>) : ArrayAdapter<PizzaStore>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.pizza_store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return  row
     }
 }
