29002b15e3c4a67fcd76dad8f3acd4c9c1401917
황선혜 <ghkdtjs6451@gmail.com>
2020-07-18 14:50:59 +0900

    페이지별 제목 띄우기

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,13 @@ import kr.co.tjoeun.pizzastore_20200718.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter (fm:FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "피자주문"
+            else -> "내 정보 설정"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position){
             0 -> PizzaStoreListFragment()
