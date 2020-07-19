5cd6b4fb352b96608e3b07ff29b59042097cc4af
황선혜 <ghkdtjs6451@gmail.com>
2020-07-19 10:23:37 +0900

    startActivityForResult로 변경 하는거 만들었대

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
@@ -1,14 +1,19 @@
 package kr.co.tjoeun.pizzastore_20200718.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kr.co.tjoeun.pizzastore_20200718.EditNickNameActivity
 import kr.co.tjoeun.pizzastore_20200718.R
 
 class MyProfileFragment:Fragment() {
+
+//    닉네임 변경 요청임을 구분하기 위한 숫자 저장 변수
+    val REQ_FOR_NICKNAME = 2003
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +28,8 @@ class MyProfileFragment:Fragment() {
 
         changeNickNameBtn.setOnClickListener {
 
-
+            val myIntent = Intent(activity,EditNickNameActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
 
         }
 
